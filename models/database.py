19f78cb619301eaa139443c370d6c266db3bfb89
Ivan_Jobs <ivanvasilev077@gmail.com>
2025-08-15 18:51:22 +0300

    database_url

--- models/database.py
+++ models/database.py
@@ -4,9 +4,6 @@ import databases
 from dotenv import load_dotenv
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 database = databases.Database(SQLALCHEMY_DATABASE_URL)
